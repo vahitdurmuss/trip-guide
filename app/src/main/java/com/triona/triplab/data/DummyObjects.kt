ce7024d6b24184c4c38afdd52d368c0e7d96092f
vahit.durmus <vahit.durmus@obss.tech>
2024-07-17 22:19:56 +0300

    add dummy cities data

--- app/src/main/java/com/triona/triplab/data/DummyObjects.kt
+++ app/src/main/java/com/triona/triplab/data/DummyObjects.kt
@@ -2,8 +2,34 @@ package com.triona.triplab.data
 
 val listGuiders = List(10) { Guider(it, "Vahit", "Durmuş") }
 
-val categories = listOf("SeaSide", "Beach", "Mountain")
-val regions= listOf("Marmara", "Aegean", "Black Sea", "Central Anatolia", "Eastern Anatolia", "Southeastern Anatolia", "Mediterranean")
+val categories = listOf(
+    "SeaSide", "Beach", "Mountain", "Historical", "Cultural", "Adventure",
+    "CityBreak", "Wellness", "Gastronomy", "Nature", "Sports", "Family",
+    "Romantic", "Luxury", "Backpacking", "Cruise", "Wildlife", "Festival",
+    "Pilgrimage", "Shopping", "Photography"
+)
+val regions = listOf(
+    "Marmara",
+    "Aegean",
+    "Black Sea",
+    "Central Anatolia",
+    "Eastern Anatolia",
+    "Southeastern Anatolia",
+    "Mediterranean"
+)
+val cities = listOf(
+    "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya",
+    "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik",
+    "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum",
+    "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum",
+    "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır",
+    "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars",
+    "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli",
+    "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş",
+    "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt",
+    "Sinop", "Sivas", "Şanlıurfa", "Şırnak", "Tekirdağ", "Tokat", "Trabzon",
+    "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak"
+)
 
 val onClickTrip: (Trip) -> Unit = {}
 val onClickCategory: (String) -> Unit = {}
