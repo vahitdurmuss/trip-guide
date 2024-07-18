b65704bc81217d415a539de05c43809c053701db
vahit.durmus <vahit.durmus@obss.tech>
2024-07-18 22:01:14 +0300

    add images

--- app/src/main/java/com/triona/triplab/data/TripStep.kt
+++ app/src/main/java/com/triona/triplab/data/TripStep.kt
@@ -1,14 +1,12 @@
 package com.triona.triplab.data
 
-import java.util.Date
-
 data class TripStep(
     val title: String,
     val quickDescription: String,
-    val detailedDescription: String = "",
+    val detailedDescription: String,
     val startHour:Int,
     val endHour:Int,
     val date:String,
-    val location: String = "32,39",
-    val photo:String=""
+    val location: String,
+    val photo:String
 )
