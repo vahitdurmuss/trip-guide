7b6ec19b06500cdacef06488c7786aa3a32f902c
vahit.durmus <vahit.durmus@obss.tech>
2024-07-19 15:12:58 +0300

    change numeric hour into text time

--- app/src/main/java/com/triona/triplab/data/RizeDummyTrip.kt
+++ app/src/main/java/com/triona/triplab/data/RizeDummyTrip.kt
@@ -6,8 +6,8 @@ object RizeDummyTrip {
             title = "Sabah Kahvaltısı",
             quickDescription = "Rize'nin lezzetli kahvaltılarından keyif alın.",
             detailedDescription = "Rize'de güzel bir kahvaltı yapın ve yöresel lezzetleri deneyin.",
-            startHour = 8,
-            endHour = 9,
+            startTime = "08:00",
+            endTime = "09:00",
             date = "2024-07-25",
             location = "40.9091, 40.8933",
             photo = "kahvalti.jpg"
@@ -16,8 +16,8 @@ object RizeDummyTrip {
             title = "Botanik Park Ziyareti",
             quickDescription = "Doğanın güzelliklerini keşfedin.",
             detailedDescription = "Rize Botanik Parkı'nda yürüyüş yapın ve çeşitli bitki türlerini keşfedin.",
-            startHour = 10,
-            endHour = 12,
+            startTime = "10:00",
+            endTime = "12:00",
             date = "2024-07-25",
             location = "40.9300, 40.8889",
             photo = "botanik_park.jpg"
@@ -26,8 +26,8 @@ object RizeDummyTrip {
             title = "Çay Fabrikası Turu",
             quickDescription = "Rize'nin çay üretim sürecini öğrenin.",
             detailedDescription = "Rize çay fabrikasını ziyaret edin ve çayın üretim aşamalarını görün.",
-            startHour = 13,
-            endHour = 15,
+            startTime = "13:00",
+            endTime = "15:00",
             date = "2024-07-25",
             location = "40.9194, 40.8707",
             photo = "cay_fabrikasi.jpg"
@@ -36,8 +36,8 @@ object RizeDummyTrip {
             title = "Öğle Yemeği",
             quickDescription = "Yöresel bir restoranda öğle yemeği.",
             detailedDescription = "Rize'nin meşhur lezzetlerini tadabileceğiniz bir restoranda öğle yemeği.",
-            startHour = 12,
-            endHour = 13,
+            startTime = "12:00",
+            endTime = "13:00",
             date = "2024-07-25",
             location = "40.9047, 40.9059",
             photo = "ogle_yemegi.jpg"
@@ -46,8 +46,8 @@ object RizeDummyTrip {
             title = "Şelale Ziyareti",
             quickDescription = "Doğanın içinde serinleyin.",
             detailedDescription = "Rize'nin doğal güzelliklerinden biri olan şelaleyi ziyaret edin ve serinleyin.",
-            startHour = 15,
-            endHour = 17,
+            startTime = "15:00",
+            endTime = "17:00",
             date = "2024-07-25",
             location = "40.9561, 40.6903",
             photo = "selale.jpg"
@@ -56,8 +56,8 @@ object RizeDummyTrip {
             title = "El Sanatları Atölyesi Ziyareti",
             quickDescription = "Yöresel el sanatlarına tanıklık edin.",
             detailedDescription = "Rize'de bir el sanatları atölyesini ziyaret edin ve geleneksel el işçiliği hakkında bilgi edinin.",
-            startHour = 17,
-            endHour = 18,
+            startTime = "17:00",
+            endTime = "18:00",
             date = "2024-07-25",
             location = "40.9069, 40.9216",
             photo = "el_sanatlari.jpg"
@@ -66,8 +66,8 @@ object RizeDummyTrip {
             title = "Akşam Çay Keyfi",
             quickDescription = "Rize çayının tadını çıkarın.",
             detailedDescription = "Akşam üzeri bir çay bahçesinde Rize'nin meşhur çayını içerek keyif yapın.",
-            startHour = 18,
-            endHour = 19,
+            startTime = "18:00",
+            endTime = "19:00",
             date = "2024-07-25",
             location = "40.9084, 40.9552",
             photo = "aksam_cay.jpg"
@@ -76,8 +76,8 @@ object RizeDummyTrip {
             title = "Akşam Yemeği",
             quickDescription = "Akşam yemeği için yerel bir restorana gidin.",
             detailedDescription = "Rize'nin lezzetli yemeklerinden oluşan bir akşam yemeği için yerel bir restorana gidin.",
-            startHour = 19,
-            endHour = 20,
+            startTime = "19:00",
+            endTime = "20:00",
             date = "2024-07-25",
             location = "40.9047, 40.9059",
             photo = "aksam_yemegi.jpg"
@@ -86,8 +86,8 @@ object RizeDummyTrip {
             title = "Sahil Gezisi",
             quickDescription = "Rize'nin sahilini keşfedin.",
             detailedDescription = "Güneşin batışını izlemek için Rize'nin sahilini gezin ve manzaraya hayran kalın.",
-            startHour = 20,
-            endHour = 21,
+            startTime = "20:00",
+            endTime = "21:00",
             date = "2024-07-25",
             location = "40.9371, 40.9357",
             photo = "sahil_gezisi.jpg"
@@ -96,13 +96,14 @@ object RizeDummyTrip {
             title = "Dönüş ve Konaklama",
             quickDescription = "Otele dönüş ve geceyi dinlenerek geçirin.",
             detailedDescription = "Günün sonunda otele dönün ve dinlenmek için zaman ayırın.",
-            startHour = 21,
-            endHour = 22,
+            startTime = "21:00",
+            endTime = "22:00",
             date = "2024-07-25",
             location = "40.9063, 40.9075",
             photo = "konaklama.jpg"
         )
     )
+
 
     // Rize için Trip nesnesini oluşturma
     val rizeTrip = Trip(
--- app/src/main/java/com/triona/triplab/ui/TripStep.kt
+++ app/src/main/java/com/triona/triplab/ui/TripStep.kt
@@ -11,12 +11,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,11 +23,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.triona.triplab.R
 import com.triona.triplab.data.MuglaDummyTrip
 import com.triona.triplab.data.RizeDummyTrip
@@ -53,7 +47,7 @@ fun TripStep(tripStep: TripStep, onClickTrip: (TripStep) -> Unit, modifier: Modi
             Icon(painter = painterResource(id = R.drawable.ic_time), contentDescription = "time")
 
             Text(
-                text = "${tripStep.startHour}-${tripStep.endHour}",
+                text = "${tripStep.startTime}-${tripStep.endTime}",
                 style = MaterialTheme.typography.labelMedium,
                 textAlign = TextAlign.End
             )
